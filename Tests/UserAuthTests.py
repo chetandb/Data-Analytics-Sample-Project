e1a1a625b101e996ba2ecba5b8cc97a41ed71ba7
chetandb <chetdb@yahoo.com>
2024-07-24 21:46:40 +0530

    User management module with functional test code added

--- Tests/UserAuthTests.py
+++ Tests/UserAuthTests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 class UserAuthTests(unittest.TestCase):
 
